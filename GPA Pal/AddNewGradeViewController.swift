9dbc648c47a1fa94fcd3a3c35312fd0aaeb73dc3
Omar Olivarez <olivarez.omar551@gmail.com>
2017-11-21 18:09:25 -0600

    Changed ReadMe and added Keyboard going-away code

--- GPA Pal/AddNewGradeViewController.swift
+++ GPA Pal/AddNewGradeViewController.swift
@@ -197,6 +197,19 @@ class AddNewGradeViewController: UIViewController, UIPickerViewDelegate, UIPicke
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 'First Responder' is the same as 'input focus'.
+        // We are removing input focus from the text field.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Called when the user touches on the main view (outside the UITextField).
+    //
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     
     /*
      
